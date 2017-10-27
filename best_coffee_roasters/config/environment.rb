b95d54a101d268b7ce02dbb9cbc2462278051660
Melissa Rudick <mel.madrugada@gmail.com>
2017-10-27 19:57:38 +0000

    folders too deep - cannot access bin without ruby commmand. added config folder

---
+++ best_coffee_roasters/config/environment.rb
@@ -0,0 +1,7 @@
+require 'pry'
+require 'nokogiri'
+require 'open-uri'
+
+# require_relative '../lib/best_coffee_roasters/scraper'
+require_relative '../lib/best_coffee_roasters/roasters'
+require_relative '../lib/best_coffee_roasters/cli'
--- best_coffee_roasters/lib/best_coffee_roasters.rb
+++ best_coffee_roasters/lib/best_coffee_roasters.rb
@@ -1,5 +1,5 @@
-require "best_coffee_roasters/version"
-
+require_relative "best_coffee_roasters/version"
+require_relative '../config/environment'
 module BestCoffeeRoasters
   # Your code goes here...
 end
